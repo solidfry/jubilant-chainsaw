97e3f19ad6e8c0e5fc36e5b818110df3d1b42fa5
solidfry <ellioteweb@gmail.com>
2022-10-02 21:13:47 +1100

    book prefab

---
+++ Assets/SpellCard.cs
@@ -0,0 +1,38 @@
+using ScriptableObjects;
+using TMPro;
+using UnityEngine;
+
+public class SpellCard : MonoBehaviour
+{
+    [SerializeField] private SpellData spell;
+    [SerializeField] private TMP_Text title, description, sideBarTitle, sideBarDescription;
+
+    public SpellData Spell
+    {
+        get => spell;
+        set => spell = value;
+    }
+    
+    private void Awake()
+    {
+        ApplyData();
+    }
+    private void OnValidate()
+    {
+        ApplyData();
+    }
+
+    void ApplyData()
+    {
+        if (spell != null) {
+            title.text = spell.name;
+            description.text = spell.description;
+            sideBarDescription.text = null;
+            foreach (var ingredient in spell.Recipe)
+            {
+                sideBarDescription.text += $"{ingredient.requirement} x {ingredient.ingredientType.name}\n";
+            }
+        }
+    }
+
+}
